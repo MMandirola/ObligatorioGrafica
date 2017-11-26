b3b846edb605e524e5f50db974e6c647970329e4
marcelo <marcelo@marcelo.marcelo>
2017-11-26 13:56:30 -0300

    element_skeleton

--- Obligatorio/Template_Texturas/Template/objects/element.c
+++ Obligatorio/Template_Texturas/Template/objects/element.c
@@ -1,6 +1,42 @@
 #include "element.h"
-void initElement(Element element, char * line);
-void freeElement(Element * element);
-void renderElement(Element * element){
-	
+#include "../utils/list.h"
+#include <GL/gl.h>
+#include "animation.h"
+void initElement(Element * element, Animation * animations, int * indexs, long nanims, unsigned int texture, float rotatex, float rotatey, float rotatez, float translatex, float translatey, float translatez, float scalex, float scaley, float scalez){
+	initArrayList(sizeof(Animation *), &(element -> animations));
+	element -> texture = texture;
+	element -> rotatex = rotatex;
+	element -> rotatey = rotatey;
+	element -> rotatez = rotatez;
+	element -> translatex = translatex;
+	element -> translatey = translatey;
+	element -> translatez = translatez;
+	element -> scalex = scalex;
+	element -> scaley = scaley;
+	element -> scalez = scalez;
+	for(int i=0;i<nanims;i++){
+		int index = indexs[i];
+		Animation * anim = animations + index;
+		addElement(&anim, &(element -> animations));  
+	}
+
+}
+void freeElement(Element * element){
+	freeList(&(element -> animations));
+	free(element);
+}
+void renderElement(Element * element, float seconds){
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, element -> texture);
+	glTranslatef(element -> translatex, element -> translatey, element -> translatez);
+	glRotatef(element -> rotatex, 1, 0, 0);
+	glRotatef(element -> rotatey, 0, 1, 0);
+	glRotatef(element -> rotatez, 0, 0, 1);
+	glScalef(element -> scalex, element -> scaley, element -> scalez);
+	if ((element -> animations).list_size != 0){
+		Animation ** current = (element ->animations).vector;
+		Animation * element = current[0];
+		animationRender(element, seconds);
+	}
+	glPopMatrix();
 }
--- Obligatorio/Template_Texturas/Template/objects/element.h
+++ Obligatorio/Template_Texturas/Template/objects/element.h
@@ -8,6 +8,7 @@
 #include <SDL.h>
 #endif //APPLEMAC#include "../utils/list.h"
 #include "../utils/list.h"
+#include "animation.h"
 typedef struct{
 	ArrayList animations;
 	unsigned int texture;
@@ -21,7 +22,6 @@ typedef struct{
 	float scaley;
 	float scalez;
 } Element;
-void initElement(Element face, char * line);
-void freeElement(Element * element);
-void renderElement(Element * element);
+void initElement(Element * element, Animation * animations, int * indexs, long nanims, unsigned int texture, float rotatex, float rotatey, float rotatez, float translatex, float translatey, float translatez, float scalex, float scaley, float scalez);
+void renderElement(Element * element, float seconds);
 #endif
